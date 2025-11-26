ab1e3400695d845731f0dad656f8d3f254727c46
Gaurav Misra <keyvez@gmail.com>
2025-11-26 12:18:05 -0800

    chore: cleanup and fix lints

--- lib/pill_widget.dart
+++ lib/pill_widget.dart
@@ -1,6 +1,1 @@
-/// A customizable pill/chip widget for Flutter with inline editing support.
-///
-/// Display labels with optional editable values in a sleek pill-shaped container.
-
-
 export 'pill.dart';
