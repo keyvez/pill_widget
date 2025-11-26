e40c56ad660c4a60b2fb5a99c109f4c0d1e7e335
Gaurav Misra <keyvez@gmail.com>
2025-11-26 12:15:37 -0800

    style: remove unnecessary library directive

--- lib/pill_widget.dart
+++ lib/pill_widget.dart
@@ -1,6 +1,6 @@
 /// A customizable pill/chip widget for Flutter with inline editing support.
 ///
 /// Display labels with optional editable values in a sleek pill-shaped container.
-library pill_widget;
+
 
 export 'pill.dart';
